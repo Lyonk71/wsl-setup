39f0d1b677f486719b881ee699813c741e3117cc
Keith Lyons <lyonk71@gmail.com>
2019-04-07 12:43:31 -0400

    separated out wsl setup specific stuff

--- .download_packages.sh
+++ .download_packages.sh
@@ -3,8 +3,6 @@
 \cp /home/lyonk71/lyonk71/.vimrc ~
 \cp /home/lyonk71/lyonk71/.bashrc ~
 \cp /home/lyonk71/lyonk71/.download_packages.sh ~
-\cp /home/lyonk71/lyonk71/.ide.sh ~
-\cp -r /home/lyonk71/lyonk71/.git ~
 
 # update system
 sudo apt update -y
@@ -13,23 +11,9 @@ sudo apt upgrade -y
 # upgrade vim for xterm_clipboard support
 sudo apt install vim-gtk
 
-# install vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim || git -c http.sslVerify=false clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
-
-# install youcompleteme
-sudo apt-get install cmake -y
-sudo apt-get install python-dev python3-dev -y
-sudo apt install build-essential cmake python3-dev -y
-cd ~/.vim/bundle/YouCompleteMe
-python3 install.py
-vim +PluginInstall +qall
-
 # get pip
 sudo apt install python3-pip -y
-sudo apt install ipython3
 
-rm -rf /home/lyonk71/lyonk71
 
 
 
--- .ide.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-tmux new \; split-window \; send-keys 'clear' C-m 'ipython3' C-m \; select-pane -U \; send-keys 'clear' C-m 
-
