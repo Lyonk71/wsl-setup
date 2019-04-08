89aedf1f1347a402ebee04f8a370fe342f4a3147
Keith Lyons <lyonk71@gmail.com>
2019-04-08 10:07:10 -0400

    Update .download_packages.sh
    temporarily comment out removal of duplicate lines for bashrc & vimrc

--- .download_packages.sh
+++ .download_packages.sh
@@ -3,13 +3,13 @@
 
 # update .vimrc
 cat /home/lyonk71/wsl-setup/to_vimrc >> /home/lyonk71/.vimrc
-awk '!a[$0]++' .vimrc > .vimrctemp
-mv .vimrctemp .vimrc
+# awk '!a[$0]++' .vimrc > .vimrctemp
+# mv .vimrctemp .vimrc
 
 # update .bashrc
 cat /home/lyonk71/wsl-setup/to_bashrc >> /home/lyonk71/.bashrc
-awk '!a[$0]++' .bashrc > .bashrctemp
-mv .bashrctemp .bashrc
+# awk '!a[$0]++' .bashrc > .bashrctemp
+# mv .bashrctemp .bashrc
 
 # update system
 sudo apt update -y
