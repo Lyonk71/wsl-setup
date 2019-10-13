8bc35f9794e148b34d926c864e60ecaafd0ed243
Keith Lyons <lyonk71@gmail.com>
2019-10-13 10:10:06 -0400

    Update .download_packages.sh

--- .download_packages.sh
+++ .download_packages.sh
@@ -20,8 +20,6 @@ sudo apt install python3-pip -y
 # get ipython
 sudo apt install ipython3 -y
 
-
-
 # set up jvim --------------------------------------------------------------------
 
 mv ~/wsl-setup/.tmux.conf ~/.tmux.conf 
@@ -29,13 +27,13 @@ chmod u+x ~/wsl-setup/jvim
 sudo mv ~/wsl-setup/jvim /bin/jvim
 
 # copy F1, F2, F3, and F4 shortcuts to ~
-cp /home/lyonk71/jupyvim/.f1_script.sh ~
-cp /home/lyonk71/jupyvim/.f2_script.sh ~
-cp /home/lyonk71/jupyvim/.f3_script.sh ~
-cp /home/lyonk71/jupyvim/.f4_script.sh ~
+cp /home/lyonk71/wsl-setup/.f1_script.sh ~
+cp /home/lyonk71/wsl-setup/.f2_script.sh ~
+cp /home/lyonk71/wsl-setup/.f3_script.sh ~
+cp /home/lyonk71/wsl-setup/.f4_script.sh ~
 
 # install vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim || git -c http.sslVerify=false clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim || git -c http.sslVerify=true clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
 # install black for vim-autoformat
