d2492ae5faeec429bcb096041d46f4e9d411155c
Keith Lyons <lyonk71@gmail.com>
2019-10-13 10:11:45 -0400

    Create .f2_script.sh

---
+++ .f2_script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+tmux select-pane -t 0
+tmux send-keys ':!rm jtemp.py'
+tmux send-keys 'Enter'
+tmux send-keys 'Enter'
+
+tmux send-keys '/#'
+tmux send-keys 'Enter'
+tmux send-keys 'm]'
+tmux send-keys 'kVgg'
+tmux send-keys ':w jtemp.py'
+tmux send-keys 'Enter'
+
+tmux send-keys ':!while [ ! -f jtemp.py ]; do sleep .1; done'
+
+tmux send-keys 'Enter'
+tmux send-keys 'Enter'
+
+tmux select-pane -t 1 
+tmux send-keys '%load jtemp.py'
+tmux send-keys 'Enter'
+tmux send-keys 'Escape'
+tmux send-keys 'ddddggdd'
+tmux send-keys 'Escape'
+tmux send-keys 'Enter'
+
+tmux select-pane -t 0
+tmux send-keys ':!rm jtemp.py'
+tmux send-keys 'Enter'
+tmux send-keys 'Enter'
+tmux send-keys '`]Nj'
