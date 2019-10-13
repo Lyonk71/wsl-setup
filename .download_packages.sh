abe56258252dc75884f372ec2ae6c6317ca3990d
Keith Lyons <lyonk71@gmail.com>
2019-10-12 23:12:03 -0400

    Update .download_packages.sh

--- .download_packages.sh
+++ .download_packages.sh
@@ -2,14 +2,10 @@
 
 
 # update .vimrc
-cat /home/lyonk71/wsl-setup/to_vimrc >> /home/lyonk71/.vimrc
-# awk '!a[$0]++' .vimrc > .vimrctemp
-# mv .vimrctemp .vimrc
+mv /home/lyonk71/wsl-setup/to_vimrc >> /home/lyonk71/.vimrc
 
 # update .bashrc
 cat /home/lyonk71/wsl-setup/to_bashrc >> /home/lyonk71/.bashrc
-# awk '!a[$0]++' .bashrc > .bashrctemp
-# mv .bashrctemp .bashrc
 
 # update system
 sudo apt update -y
