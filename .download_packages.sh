9aaa8ea8c8b8dc9eb5779f10e6df8ff6c77af328
Keith Lyons <lyonk71@gmail.com>
2019-10-12 22:58:27 -0400

    Update .download_packages.sh

--- .download_packages.sh
+++ .download_packages.sh
@@ -21,6 +21,14 @@ sudo apt install vim-gtk -y
 # get pip
 sudo apt install python3-pip -y
 
+# get ipython
+sudo apt install ipython3 -y
 
+# install vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim || git -c http.sslVerify=false clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+# install black for vim-autoformat
+pip3 install black
 
 
