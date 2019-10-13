fa1baeeb66fe82ba200cf85b02c5f545e8afc956
Keith Lyons <lyonk71@gmail.com>
2019-10-13 10:11:11 -0400

    Create .f1_script.sh

---
+++ .f1_script.sh
@@ -0,0 +1,14 @@
+  
+#!/bin/bash
+
+tmux select-pane -t 1
+tmux send-keys C-c
+tmux send-keys 'exit()'
+tmux send-keys 'Enter'
+
+tmux send-keys 'ipython3 --TerminalInteractiveShell.editing_mode=vi'
+tmux send-keys 'Enter'
+tmux send-keys 'clear'
+tmux send-keys 'Enter'
+
+tmux select-pane -t 0
