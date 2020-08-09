54db7c556ba572e5ab2cb3ea5a93b054aae7b04a
Keith Lyons <keith_lyons@outlook.com>
2020-08-09 14:33:49 -0400

    fix path issues

--- .download_packages.sh
+++ .download_packages.sh
@@ -18,10 +18,10 @@ sudo apt install python3-pip -y
 sudo apt install ipython3 -y
 
 # update .vimrc
-mv $HOME/wsl-setup/.vimrc /home/lyonk71/.vimrc
+cp $HOME/wsl-setup/.vimrc /home/lyonk71/.vimrc
 
 # update .tmux.conf
-mv $HOME/wsl-setup/.tmux.conf ~/.tmux.conf 
+cp $HOME/wsl-setup/.tmux.conf ~/.tmux.conf 
 
 # install vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim || git -c http.sslVerify=false clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -31,7 +31,7 @@ vim +PluginInstall +qall
 git submodule update --init --recursive ~/.vim/bundle/jedi-vim
 
 # copy F1, F2, F3, and F4 shortcuts to ~
-cp /home/lyonk71/wsl-setup/.f1_script.sh ~
-cp /home/lyonk71/wsl-setup/.f2_script.sh ~
-cp /home/lyonk71/wsl-setup/.f3_script.sh ~
-cp /home/lyonk71/wsl-setup/.f4_script.sh ~
+cp $HOME/wsl-setup/.f1_script.sh ~
+cp $HOME/wsl-setup/.f2_script.sh ~
+cp $HOME/wsl-setup/.f3_script.sh ~
+cp $HOME/wsl-setup/.f4_script.sh ~
