6632f7646bb480e10ca9c53ecf4738767129841a
Keith Lyons <lyonk71@gmail.com>
2020-08-16 21:35:08 -0400

    Update .f1_script.sh
    Command had been running too fast for keystrokes.

--- .f1_script.sh
+++ .f1_script.sh
@@ -5,6 +5,7 @@ tmux select-pane -t 1
 tmux send-keys C-c
 tmux send-keys 'exit()'
 tmux send-keys 'Enter'
+sleep .25
 
 tmux send-keys 'ipython3 --TerminalInteractiveShell.editing_mode=vi'
 tmux send-keys 'Enter'
